fde90c135c5f1cc72bd95f786b285abfbcb50c4b
Brayan Bautista <brayan.bautista@ibit.pe>
2024-08-26 17:57:42 -0500

    22

---
+++ hola.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <div>
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "Hola vue",
+    };
+  },
+};
+</script>
+
+<style>
+p {
+  color: red;
+}
+</style>
